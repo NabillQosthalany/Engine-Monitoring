e7ab771174fb51506386c7483637c4b223e8d123
ayyuuf <ayuufebrii92@gmail.com>
2022-10-12 19:36:32 +0700

    + add navbar

---
+++ src/views/dashboard/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app id="inspire">
+        <SideBar/>
+   </v-app>
+  
+  </template>
+
+
+  <script>
+  import SideBar from '@/components/SideBar';
+
+  export default {
+    name: 'Home',
+    components: {
+      SideBar
+  },
+  }
+  </script>
