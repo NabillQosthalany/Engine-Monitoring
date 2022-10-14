baaf566ab0a7cb10f65c9ba0a061a4c36243904f
ayyuuf <ayuufebrii92@gmail.com>
2022-10-14 11:53:36 +0700

    add sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -9,13 +9,14 @@
                <v-img class="mx-auto" src="@/assets/logo.png" height="80px" width="200px" ></v-img>
                <v-divider color="white" class="mt-4"></v-divider>
             </div>
-            <v-list   dense class="mt-5" >
+            <v-list dense class="mt-5" >
                 <v-list-item-group v-model="selectedItem" color="white" >
 
                     <v-list-item v-for="(item, i) in items" :key="i" v-slot="{ active }">
                         <v-list-item-icon class="ml-6">
                             <v-icon v-text="item.icon"  :color="active ? 'black' : 'white'" ></v-icon>
                         </v-list-item-icon>
+
                         <v-list-item-content>
                             <v-list-item-title v-text="item.text" :class="active ? 'black--text' : 'white--text'" ></v-list-item-title>
                         </v-list-item-content>
